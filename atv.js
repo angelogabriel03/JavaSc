31b1863cb9e293fc23a923911875a6d4dfb2bc02
angelogabriel03 <129082662+angelogabriel03@users.noreply.github.com>
2023-04-10 09:39:56 -0300

    Add files via upload

---
+++ atv.js
@@ -0,0 +1,102 @@
+    /*for (let i = 0; i <=10; i++) {
+    console.log(i)
+}*/
+
+/* atv 2
+/*var n = prompt ("Digite um numero");
+var total = n/2
+
+if (n & 1) {
+    alert("Impar")
+} else{
+    alert ("Par")
+}*/
+
+/*Atv 3
+var a = +prompt ("Informe o primeiro numero")
+var b = +prompt ("Informe o segundo numero")
+
+var media =((a + b) /2)
+        console.log("Media è", media)*/
+
+    
+
+/*Atv4
+
+var Numero1 = prompt ("Informe o primeiro numero")
+var Numero2 = prompt ("Informe o segundo numero")
+var Numero3 = prompt ("Informe o terceiro numero")
+var Numero4 = prompt ("Informe o quarto numero")
+var Numero5 = prompt ("Informe o quinto numero")
+
+console.log(Math.max(Numero1, Numero2, Numero3, Numero4, Numero5))*/
+
+/* Atv5
+
+var Array01 = prompt ("Informe o primeiro numero")
+var Array02 = prompt ("Informe o segundo numero")
+var Array03 = prompt ("Informe o terceiro numero")
+var ArrayTotal = Array01+Array02+Array03;
+
+console.log("ArrayTotal:"+ArrayTotal.length)*/
+
+
+/* Atv 6
+
+let list = ["smk", "smoke", "fumaca", "angelo"];
+list.splice(1, 2)
+console.log(list)*/
+
+/*Atv 7
+
+let frase = "Copa do Mundo"
+let inverter = frase.split("").reverse().join("");
+
+console.log(inverter);*/
+
+/* Atv 8 
+
+var palíndromo = prompt("Escreva uma palavra")
+let inverter = palíndromo.split("").reverse().join("");
+
+if (inverter == palíndromo){
+    alert ("E um palíndromo")
+} else{
+    alert("Não e um palíndromo")
+} */
+
+/* Atv 9 
+
+var fatorial = +prompt("informe um numero")
+var resultado = 1;
+for (var i = fatorial; i > 1 ; i--){
+resultado *=i;
+}
+console.log(resultado);*/
+
+/* Atv 10
+
+var numero1 = 1
+var numero2 = numero1+numero1
+var numero3 = numero2+numero1
+var numero4 = numero3+numero2
+var numero5 = numero4+numero3
+var numero6 = numero5+numero4
+var numero7 = numero6+numero5
+var numero8 = numero7+numero6
+var numero9 = numero8+numero7
+
+console.log(numero2)
+console.log(numero3)
+console.log(numero4)
+console.log(numero5)
+console.log(numero6)
+console.log(numero7)
+console.log(numero8)
+console.log(numero9)
+*/
+
+
+
+
+
